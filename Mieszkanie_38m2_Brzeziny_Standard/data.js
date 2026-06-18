0c70c84e842812917cd2d1bf62fda0d37889b897
Vizprojects360 <155273263+Vizprojects360@users.noreply.github.com>
2026-06-18 14:39:35 +0200

    Upload

---
+++ Mieszkanie_38m2_Brzeziny_Standard/data.js
@@ -0,0 +1,341 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-przedpokj",
+      "name": "Przedpokój",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10257639207195446,
+          "pitch": 0.31255489574963846,
+          "rotation": 0,
+          "target": "1-sypialnia"
+        },
+        {
+          "yaw": 3.0361151068158367,
+          "pitch": 0.7740635120574453,
+          "rotation": 0,
+          "target": "2-salon_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sypialnia",
+      "name": "Sypialnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4055139095954665,
+          "pitch": 0.17677267680690534,
+          "rotation": 0,
+          "target": "0-przedpokj"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-salon_1",
+      "name": "Salon_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2285834052035547,
+          "pitch": 0.6984339472630463,
+          "rotation": 0,
+          "target": "0-przedpokj"
+        },
+        {
+          "yaw": 2.3226111137929415,
+          "pitch": 0.5831537500988446,
+          "rotation": 0,
+          "target": "3-salon_2"
+        },
+        {
+          "yaw": -1.5753233649849427,
+          "pitch": 0.2862876439125692,
+          "rotation": 0,
+          "target": "5-azienka"
+        },
+        {
+          "yaw": -2.939302932523214,
+          "pitch": 0.5574688579003002,
+          "rotation": 0,
+          "target": "4-kuchnia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-salon_2",
+      "name": "Salon_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6064767502128987,
+          "pitch": 0.263778121194715,
+          "rotation": 0,
+          "target": "4-kuchnia"
+        },
+        {
+          "yaw": 1.6262210532257892,
+          "pitch": 0.5764420952733786,
+          "rotation": 0,
+          "target": "2-salon_1"
+        },
+        {
+          "yaw": -1.315567624314589,
+          "pitch": 0.5160279379934707,
+          "rotation": 0,
+          "target": "6-balkon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-kuchnia",
+      "name": "Kuchnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6114134613269826,
+          "pitch": 0.808800612706964,
+          "rotation": 0,
+          "target": "2-salon_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-azienka",
+      "name": "Łazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6006115369511704,
+          "pitch": 0.23116258134129097,
+          "rotation": 0,
+          "target": "2-salon_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-balkon",
+      "name": "Balkon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5846848900171553,
+          "pitch": 0.520113619113328,
+          "rotation": 0,
+          "target": "3-salon_2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Mieszkanie 38m2 - Brzeziny - Standard",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};

a0c34576439997b0e22c5f4c6ffbd4cd731b8c52
Vizprojects360 <155273263+Vizprojects360@users.noreply.github.com>
2026-06-18 17:23:44 +0200

    Upload

---
+++ Apartament_Łódź/data.js
@@ -0,0 +1,347 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-strefa-wejcia",
+      "name": "Strefa wejścia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.959401222341188,
+          "pitch": 0.7585744230709288,
+          "rotation": 0,
+          "target": "3-przedpokj"
+        },
+        {
+          "yaw": -1.1218043773894735,
+          "pitch": 0.20274206571601816,
+          "rotation": 0,
+          "target": "1-salon-z-kuchni"
+        },
+        {
+          "yaw": 0.054419620387205825,
+          "pitch": 0.3042979167115778,
+          "rotation": 0,
+          "target": "2-azienka-z-saun"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-salon-z-kuchni",
+      "name": "Salon z kuchnią",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.671096377242943,
+          "pitch": 0.19361278014417316,
+          "rotation": 0,
+          "target": "6-balkon"
+        },
+        {
+          "yaw": 2.3498451148571595,
+          "pitch": 0.20269299140444197,
+          "rotation": 0,
+          "target": "0-strefa-wejcia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-azienka-z-saun",
+      "name": "Łazienka z sauną",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.844920981616772,
+          "pitch": 0.120870759897592,
+          "rotation": 0,
+          "target": "3-przedpokj"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-przedpokj",
+      "name": "Przedpokój",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2491325714203754,
+          "pitch": 0.42216755031900277,
+          "rotation": 0,
+          "target": "5-sypialnia"
+        },
+        {
+          "yaw": -0.13226898621827132,
+          "pitch": 0.19261959009120133,
+          "rotation": 0,
+          "target": "4-garderoba"
+        },
+        {
+          "yaw": 2.9952075987021765,
+          "pitch": 0.7949233719589941,
+          "rotation": 0,
+          "target": "0-strefa-wejcia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-garderoba",
+      "name": "Garderoba",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0459033252403405,
+          "pitch": 0.3222832695295992,
+          "rotation": 0,
+          "target": "3-przedpokj"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sypialnia",
+      "name": "Sypialnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6067259167503511,
+          "pitch": 0.14675402139637406,
+          "rotation": 0,
+          "target": "3-przedpokj"
+        },
+        {
+          "yaw": 1.3028227289949506,
+          "pitch": 0.37610308283775495,
+          "rotation": 0,
+          "target": "6-balkon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-balkon",
+      "name": "Balkon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2752,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0752027167212095,
+          "pitch": 0.22537260347649024,
+          "rotation": 0,
+          "target": "1-salon-z-kuchni"
+        },
+        {
+          "yaw": -1.0233766620037823,
+          "pitch": 0.13001988612226434,
+          "rotation": 0,
+          "target": "5-sypialnia"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Apartament - Łódź",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
